640bfbdb0752b24c2c859f846055fc8f01c10f47
RajShree1854 <Rajshree1854@users.noreply.github.com>
2026-07-15 22:28:47 +0530

    Refactored local storage caching mechanisms
    - Cleaned up obsolete code
    - Optimized memory usage
    - Improved logging verbosity

---
+++ llmcache/utils/log.py
@@ -0,0 +1,8 @@
+﻿import logging
+
+import LLMCache
+
+FORMAT = '%(asctime)s - %(thread)d - %(filename)s-%(module)s:%(lineno)s - %(levelname)s: %(message)s'
+logging.basicConfig(format=FORMAT)
+
+llmcache_log = logging.getLogger(f'LLMCache:{LLMCache.__version__}')
